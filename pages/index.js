d418f01e9b43a0fbdea7f0f68dd9abaf1f376c44
sunnyWind <1749790593@qq.com>
2022-03-17 10:47:54 +0800

    nav

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
+import { Tabs } from '@geist-ui/core'
 import React from 'react'
 
 export default function index() {
-  return (
-    <h1>Skate Station</h1>
-  )
+    return (
+        <>
+            <Tabs initialValue='1'>
+                <Tabs.Item label='主页' value='1'>
+                    <h1 >Skate Station</h1>
+
+
+
+
+                </Tabs.Item>
+                <Tabs.Item label='资讯' value='2'>
+                </Tabs.Item>
+                <Tabs.Item label='板技' value='3'>
+                </Tabs.Item>
+                <Tabs.Item label='交流' value='4'>
+                </Tabs.Item>
+                <Tabs.Item label='我们' value='5'>
+                </Tabs.Item>
+
+            </Tabs>
+        </>
+    )
 }

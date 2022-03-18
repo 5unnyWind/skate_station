6d580e4d00686eb23777b327dc7fabcd0cc58171
sunnyWind <1749790593@qq.com>
2022-03-18 11:33:30 +0800

    :art: 封装 BaseCard

--- pages/index.js
+++ pages/index.js
@@ -22,19 +22,25 @@ export default function index() {
     const theme = useTheme()
     const classes = useStyles()
 
+    const BaseCard = ({ to, children }) => {
+        return <Card hoverable>
 
-    const Home = () => {
-        return <>
-            <Spacer></Spacer>
-            <div className={classes.card}>
-
-                <Image className={classes.image} src='0.gif' width={100} ></Image>
-                <div className={classes.mask}></div>
-                <h1 className={classes.centerText}># Skate Station</h1>
+            <div>
+                {children}
+                <Link href='javascript:;' onClick={() => { setTab(to) }} block >
+                    <Text b className={classes.row} >
+                        详细了解
+                        <Spacer></Spacer>
+                        <ArrowRightCircle></ArrowRightCircle>
+                    </Text>
+                </Link>
             </div>
-            <Spacer h={5}>
-            </Spacer>
-            <Card hoverable>
+        </Card>
+    }
+
+    const SkillsCard = () => {
+        return (
+            <BaseCard to='/skills'>
                 <div className={classes.row}>
                     <div>
 
@@ -42,11 +48,12 @@ export default function index() {
                         <Divider style={{ width: '60%' }}></Divider>
                         <Text style={{ color: theme.palette.accents_4 }}>人在家中躺 板技心中涨</Text>
                     </div>
-                    <div style={{ 'max-width': '80%', marginLeft: 25 }}>
+                    <div style={{ maxWidth: '80%', marginLeft: 25 }}>
                         <Collapse.Group>
                             <Collapse title="滑板常识" subtitle='INTRODUCTION'>
                                 <Text>HTML（超文本标记语言——HyperText Markup Language）是构成 Web 世界的一砖一瓦。
-                                    它定义了网页内容的含义和结构。除 HTML 以外的其它技术则通常用来描述一个网页的表现与展示效果（如 CSS），或功能与行为（如 JavaScript）。</Text>
+                                    它定义了网页内容的含义和结构。除 HTML 以外的其它技术则通常用来描述一个网页的表现与展示效果（如 CSS），
+                                    或功能与行为（如 JavaScript）。</Text>
                             </Collapse>
                             <Collapse title="平地" subtitle='FLATGROUND'>
                                 <Text>花式動作為主，玩法較趨近於一般街頭短板的方式，以翻板(Flip)，轉板(Shovit)等等動作為主
@@ -61,16 +68,24 @@ export default function index() {
                         </Collapse.Group>
                     </div>
                 </div>
-                <div>
-                    <Link href='javascript:;' onClick={() => { setTab('/skills')}} block >
-                        <Text b className={classes.row} >
-                            详细了解
-                            <Spacer></Spacer>
-                            <ArrowRightCircle></ArrowRightCircle>
-                        </Text>
-                    </Link>
-                </div>
-            </Card>
+            </BaseCard>
+        )
+    }
+
+
+
+    const Home = () => {
+        return <>
+            <Spacer></Spacer>
+            <div className={classes.card}>
+
+                <Image className={classes.image} src='0.gif' width={100} ></Image>
+                <div className={classes.mask}></div>
+                <h1 className={classes.centerText}># Skate Station</h1>
+            </div>
+            <Spacer h={5}>
+            </Spacer>
+            <SkillsCard></SkillsCard>
         </>
     }
 

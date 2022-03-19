45501e14c77437a4e22149d7fca2b3bd54ead600
sunnyWind <1749790593@qq.com>
2022-03-19 22:21:50 +0800

    :sparkles: add ActiCard

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Image, useTheme, Card, Spacer, Text, Divider, Collapse, Link } from '@geist-ui/core'
+import { Image, useTheme, Card, Spacer, Text, Divider, Collapse, Link, Dot, User, Tooltip, Loading } from '@geist-ui/core'
 import { ArrowRightCircle } from '@geist-ui/icons'
 
 import makeStyles from '../utils/makeStyles'
+import { Loader } from '@geist-ui/icons'
 
 
 export default function index() {
@@ -102,24 +103,20 @@ export default function index() {
                         <Divider style={{ width: '60%' }}></Divider>
                         <Text style={{ color: theme.palette.accents_4 }}>拿起滑板 抢回街道</Text>
                     </div>
-                    <div style={{ maxWidth: '80%', marginLeft: 35 }}>
-                        <Collapse.Group>
-                            <Collapse title="滑板常识" subtitle='INTRODUCTION'>
-                                <Text>HTML（超文本标记语言——HyperText Markup Language）是构成 Web 世界的一砖一瓦。
-                                    它定义了网页内容的含义和结构。除 HTML 以外的其它技术则通常用来描述一个网页的表现与展示效果（如 CSS），
-                                    或功能与行为（如 JavaScript）。</Text>
-                            </Collapse>
-                            <Collapse title="平地" subtitle='FLATGROUND'>
-                                <Text>花式動作為主，玩法較趨近於一般街頭短板的方式，以翻板(Flip)，轉板(Shovit)等等動作為主
-                                    剛入門滑行開始
-                                    許多人最常碰到的風格應該就屬freestyle了
-                                    平地花式玩法是不侷限於地形
-                                    平地花式的招式沒有捷徑、但只要慢慢的循序漸進
-                                    從最基本的開始練習
-                                    我们相信你也能感受到那種成招的喜悅。</Text>
-
-                            </Collapse>
-                        </Collapse.Group>
+                    <div style={{ width: '80%', marginLeft: 35 }}>
+                        <Divider type='default'>正在进行的活动</Divider>
+                        <div style={{ display: "flex", width:'100%',flexDirection: 'row' }}>
+                            <Dot type='success' />
+                            <User src='avator.jpg' name='Aye'></User>
+                            <Text> 正在八一广场直播过6立</Text>
+                            <Text style={{ color: theme.palette.accents_4 ,flex:1,textAlign:'end'}}> 2022/3/19 </Text>
+                        </div>
+                        <Spacer></Spacer>
+                        <Divider type='default'>已经结束的活动</Divider>
+                        <Text style={{textAlign:'center'}}>
+                            <Spacer></Spacer>
+                            <Loading />    
+                        </Text> 
                     </div>
                 </div>
             </BaseCard>
@@ -137,8 +134,8 @@ export default function index() {
                     <h1 className={classes.centerText}># Skate Station</h1>
                 </div>
                 <Spacer h={5} />
-                {/* <ActiCard />
-                <Spacer h={5} /> */}
+                <ActiCard />
+                <Spacer h={5} />
                 <SkillsCard />
                 <Spacer h={5} />
                 <NewsCard />

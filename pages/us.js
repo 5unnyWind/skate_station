6213e912441277461f82ed67c47aeb3329fca5bf
sunnyWind <1749790593@qq.com>
2022-03-20 23:00:33 +0800

    :sparkles: 给自己加个 Tooltip

--- pages/us.js
+++ pages/us.js
@@ -1,5 +1,5 @@
 import makeStyles from '../utils/makeStyles'
-import { Image, Note, Divider, Text } from '@geist-ui/core'
+import { Image, Note, Divider, Text, User, Tooltip } from '@geist-ui/core'
 import LittlePrince from '../components/LittlePrince'
 
 
@@ -8,6 +8,18 @@ export default function us() {
     return (
         <div className={classes.content}>
             <LittlePrince />
+            <Text b i style={{ letterSpacing: '0.6px', display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
+                <Text span type="success">D</Text>
+                <Text span type="warning">e</Text>
+                <Text span type="secondary">s</Text>
+                <Text span type="error">i</Text>
+                <Text span style={{ color: '#ccc' }}>g</Text>
+                <Text span type="warning">n</Text>
+                <Text span type="success" ml="5px">By</Text>
+                <Tooltip text='sunny-wind@qq.com' placement='bottom'>
+                    <User style={{ position: 'relative', top: 5 }} src='https://gitee.com/sunny_f/img-bed/raw/master/tinytf.jpg' name='听枫' ></User>
+                </Tooltip>
+            </Text>
         </div>
     )
 }

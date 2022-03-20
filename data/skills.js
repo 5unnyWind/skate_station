6635579b2fecbedddcd63969068be1d1d0a1395c
sunnyWind <1749790593@qq.com>
2022-03-20 21:59:18 +0800

    feat: 更新一张图

--- data/skills.js
+++ data/skills.js
@@ -12,7 +12,7 @@ export default {
             videoName: '脚位',
             number: 'BV1Li4y1t7VS',
             videoUrl: 'https://www.bilibili.com/video/BV1Li4y1t7VS',
-            posterUrl: 'https://gitee.com/sunny_f/img-bed/raw/master/0240577a8415bdc19a774cebf2ed54cb43ea3752.jpg@672w_378h_1c_100q.jpg'
+            posterUrl: 'https://gitee.com/sunny_f/img-bed/raw/master/Untitled-design.png'
         },
         {
             videoName: '上板滑行',

f9e7a92da8f8873deb32033183417f7d74eebba2
sunnyWind <1749790593@qq.com>
2022-03-20 15:44:09 +0800

    feat: 封装 VideoCard

--- pages/skills.js
+++ pages/skills.js
@@ -7,6 +7,22 @@ import makeStyles from '../utils/makeStyles'
 export default function skills() {
   const classes = useStyles()
 
+  const VideoCard = ({
+    videoName = '',
+    number = '',
+    videoUrl = '',
+    posterUrl = '',
+  }) => {
+    return (
+      <Card className={classes.card} h={10} hoverable style={{ backgroundImage: `url(${posterUrl})`, backgroundSize: 'cover' ,marginBottom:'10px'}}>
+        <Link href={videoUrl}>
+          <div className={classes.mask}></div>
+          <Text h3 className={classes.centerText}>{videoName}</Text>
+        </Link>
+      </Card>
+    )
+  }
+
   return (
     <>
       <div className={[classes.content]}>
@@ -14,13 +30,7 @@ export default function skills() {
         <Divider h={5} >关于滑行</Divider>
         <Spacer />
 
-
-        <Card className={classes.card} h={10} hoverable style={{ backgroundImage: `url(${data['滑行'][0].posterUrl})`, backgroundSize: 'cover' }}>
-          <Link href={data['滑行'][0].videoUrl}>
-            <div className={classes.mask}></div>
-            <Text h3 className={classes.centerText}>{data['滑行'][0].videoName}</Text>
-          </Link>
-        </Card>
+        {data['滑行'].map((v, key) => (<VideoCard {...v} key={key} />))}
 
       </div>
     </>
@@ -37,10 +47,9 @@ const useStyles = makeStyles(theme => ({
   },
   card: {
     width: '100%',
-    height: 20,
     position: 'relative',
     overflow: 'hidden',
-    cursor: 'pointer'
+    marginBottom: '100px'
   },
   mask: {
     position: 'absolute',

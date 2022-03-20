b284ce14c268447a425bd4cfb0d33659ff6372af
sunnyWind <1749790593@qq.com>
2022-03-20 21:19:29 +0800

    feat: 给还没有写的页加点说明

---
+++ pages/us.js
@@ -0,0 +1,23 @@
+import makeStyles from '../utils/makeStyles'
+import { Image, Note, Divider, Text } from '@geist-ui/core'
+
+export default function us() {
+    const classes = useStyles()
+    return (
+        <div className={classes.content}>
+            <Note type="warning" label='CONSTRUCTING'>别看了，这页还没写</Note>
+            <Image src='littlePrince.png' w={100} ></Image>
+            <Divider>Fin.</Divider>
+            <Text></Text>
+        </div>
+    )
+}
+const useStyles = makeStyles(theme => ({
+    content: {
+        width: theme.layout.pageWidthWithMargin,
+        maxWidth: "100%",
+        boxSizing: "border-box",
+        margin: "0 auto",
+        padding: `0 ${theme.layout.pageMargin}`,
+    },
+}))

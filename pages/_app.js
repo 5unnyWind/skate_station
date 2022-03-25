834268c6d11d89d73886ba3f8b839cfb867abc0e
sunnyWind <1749790593@qq.com>
2022-03-25 10:55:04 +0800

    feat: 添加 Head 优化 SEO

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { GeistProvider, CssBaseline, Themes, useTheme, useTabs } from '@geist-ui
 import '../styles/globals.css'
 import Menu from '../components/Menu/Menu'
 import { JssProvider } from 'react-jss'
+import Head from 'next/head'
 
 
 
@@ -15,12 +16,23 @@ function MyApp({ Component, pageProps }) {
   })
 
   return (
-    <JssProvider>
-      <GeistProvider themeType='ayu-dark' themes={[ayu]}>
-        <CssBaseline />
-        <Menu></Menu>
-        <Component {...pageProps} />
-        <style global jsx>{`
+    <>
+      <Head>
+        <title>Gothic Skate · NCU Skate Station</title>
+        <meta name="title" content="Gothic Skate · NCU Skate Station" />
+        <meta name="description" content="NCU Gothic 滑板小站" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://skate-station.vercel.app/" />
+        <meta property="og:title" content="Gothic Skate · NCU Skate Station" />
+        <meta property="og:description" content="NCU Gothic 滑板小站" />
+      </Head>
+      <JssProvider>
+        <GeistProvider themeType='ayu-dark' themes={[ayu]}>
+          <CssBaseline />
+          <Menu></Menu>
+          <Component {...pageProps} />
+          <style global jsx>{`
           html {
             --geist-page-nav-height: 64px;
           }
@@ -33,8 +45,10 @@ function MyApp({ Component, pageProps }) {
             border-radius: ${theme.layout.radius};
           }
           `}</style>
-      </GeistProvider>
-    </JssProvider>
+        </GeistProvider>
+      </JssProvider>
+    </>
+
   )
 }
 
